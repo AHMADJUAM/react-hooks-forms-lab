b6a8b4b9cdbe3f18e09bf4ffb65c7cdc48c270bc
ahmad juma <juam17020@gmail.com>
2024-03-09 22:03:32 +0300

    Completed assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({handleChange, addItemValue, addCategoryValue,onItemFormSubmit}) {
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: addItemValue,
+    category: addCategoryValue,
+  };
+
   return (
-    <form className="NewItem">
+    <form className="NewItem"  onSubmit={(e) => {e.preventDefault();
+       onItemFormSubmit(newItem);
+      }}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="addItem"  onChange={handleChange} value={addItemValue}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="addCategory"  onChange={handleChange} value={addCategoryValue}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
